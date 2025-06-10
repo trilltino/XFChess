49d031bb026c94fe25678b6d08132931a2ccf164
Tino <isichei@outlook.com>
2025-06-10 22:16:37 +0100

    cleaned up code

--- src/board.rs
+++ src/board.rs
@@ -1,50 +1,45 @@
- use bevy::prelude::*;
- use crate::pointer_events::{update_squarematl_on, revert_squarematl_on};
- use crate::board_utils::{Square, SquareMaterials, ReturnMaterials};
-
-
-
-
+    use bevy::prelude::*;
+    use crate::pointer_events::{update_squarematl_on, revert_squarematl_on};
+    use crate::board_utils::{Square, SquareMaterials, ReturnMaterials};
 
     #[derive(Resource, Component)]
-     pub struct Board;
+    pub struct Board;
 
-       pub fn create_board(
-            mut commands: Commands,
-            mut meshes: ResMut<Assets<Mesh>>, 
-            materials: Res<SquareMaterials>,
-            return_materials: Res<ReturnMaterials>,
-        ) { 
-            let boardmesh = meshes.add(Plane3d::default().mesh().size(1.0, 1.0));
-            for i in 0..8 {
-                for j in 0..8 {
-                    let square = Square { x: i, y: j };
-                    let material = if (i + j) % 2 == 0 {
-                        materials.white_color.clone()
-                    } else {
-                        materials.black_color.clone()
-                    };           
-                    commands.spawn((
-                        Mesh3d(boardmesh.clone()),
-                        MeshMaterial3d(material.clone()),
-                        Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
-                        Square { x: i, y: j },
-                        Board,
-                    ))
-                    // utilites for changing entity color 
-                    .observe(update_squarematl_on::<Pointer<Over>>(materials.hover_matl.clone()))
-                    .observe(revert_squarematl_on::<Pointer<Out>>(return_materials.get_original_material(&square, &materials)));
-                  }   
-                }
+    pub fn create_board(
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>, 
+        materials: Res<SquareMaterials>,
+        return_materials: Res<ReturnMaterials>,
+    ) { 
+        let boardmesh = meshes.add(Plane3d::default().mesh().size(1.0, 1.0));
+        for i in 0..8 {
+            for j in 0..8 {
+                let square = Square { x: i, y: j };
+                let material = if (i + j) % 2 == 0 {
+                    materials.white_color.clone()
+                } else {
+                    materials.black_color.clone()
+                };           
+                commands.spawn((
+                    Mesh3d(boardmesh.clone()),
+                    MeshMaterial3d(material.clone()),
+                    Transform::from_translation(Vec3::new(i as f32, 0., j as f32)),
+                    Square { x: i, y: j },
+                    Board,
+                ))
+                // utilites for changing entity color 
+                .observe(update_squarematl_on::<Pointer<Over>>(materials.hover_matl.clone()))
+                .observe(revert_squarematl_on::<Pointer<Out>>(return_materials.get_original_material(&square, &materials)));
+                }   
             }
+        }
 
-    
-                pub struct BoardPlugin;
-                    impl Plugin for BoardPlugin {
-                        fn build(&self, app: &mut App) {
-                                app.add_systems(Startup, create_board);
-                        }
-                    }
+    pub struct BoardPlugin;
+        impl Plugin for BoardPlugin {
+            fn build(&self, app: &mut App) {
+                    app.add_systems(Startup, create_board);
+            }
+        }
                 
                 
 
--- src/board_utils.rs
+++ src/board_utils.rs
@@ -1,22 +1,18 @@
-use bevy::color::palettes::tailwind::*;
-use bevy::prelude::*;
-use crate::pieces::{Piece,PieceColor};
-
-
+    use bevy::color::palettes::tailwind::*;
+    use bevy::prelude::*;
+    use crate::pieces::{Piece,PieceColor};
 
     #[derive(Default, Resource, Component, Debug, Clone, Eq, PartialEq)]
-     pub struct Square {
+    pub struct Square {
             pub x: u8,
             pub y: u8,
         }
 
-        impl Square {
-            fn is_white(&self) -> bool {
-                (self.x + self.y + 1) % 2 == 0
-            }
+    impl Square {
+        fn is_white(&self) -> bool {
+            (self.x + self.y + 1) % 2 == 0
         }
-
-
+    }
 
         
     #[derive(Resource, Component)]
@@ -26,16 +22,16 @@ use crate::pieces::{Piece,PieceColor};
             pub hover_matl:  Handle<StandardMaterial>,
         }
 
-        impl FromWorld for SquareMaterials {
-            fn from_world(world: &mut World) -> Self {
-                let mut materials = world.get_resource_mut::<Assets<StandardMaterial>>().unwrap();
-                SquareMaterials {
-                    black_color: materials.add(Color::WHITE),
-                    white_color: materials.add(Color::BLACK),
-                    hover_matl : materials.add(Color::from(AMBER_100)),
-                }
+    impl FromWorld for SquareMaterials {
+        fn from_world(world: &mut World) -> Self {
+            let mut materials = world.get_resource_mut::<Assets<StandardMaterial>>().unwrap();
+            SquareMaterials {
+                black_color: materials.add(Color::WHITE),
+                white_color: materials.add(Color::BLACK),
+                hover_matl : materials.add(Color::from(AMBER_100)),
             }
         }
+    }
 
 
 
@@ -53,64 +49,64 @@ use crate::pieces::{Piece,PieceColor};
         
 
         
-        #[derive(Debug, Clone, Resource, Component, Asset, Reflect)]
-            pub struct PieceMaterials {
-                pub hover_entmatl: Handle<StandardMaterial>,
-                pub white_material: Handle<StandardMaterial>,
-                pub black_material: Handle<StandardMaterial>,
-            }
+    #[derive(Debug, Clone, Resource, Component, Asset, Reflect)]
+        pub struct PieceMaterials {
+            pub hover_entmatl: Handle<StandardMaterial>,
+            pub white_material: Handle<StandardMaterial>,
+            pub black_material: Handle<StandardMaterial>,
+        }
   
 
-        #[derive(Debug, Clone, Resource)]
-        pub struct ReturnEntityMaterials;
+    #[derive(Debug, Clone, Resource)]
+    pub struct ReturnEntityMaterials;
 
-        impl Default for ReturnEntityMaterials {
-            fn default() -> Self {
-                ReturnEntityMaterials
+    impl Default for ReturnEntityMaterials {
+        fn default() -> Self {
+            ReturnEntityMaterials
+        }
+    }
+
+    impl ReturnEntityMaterials {
+        pub fn get_original_entmaterial(&self, piece: &Piece, materials: &PieceMaterials) -> Handle<StandardMaterial> {
+            if piece.color == PieceColor::White {
+                materials.white_material.clone()
+            } else {
+                materials.black_material.clone()
             }
         }
+    }
 
-        impl ReturnEntityMaterials {
-            pub fn get_original_entmaterial(&self, piece: &Piece, materials: &PieceMaterials) -> Handle<StandardMaterial> {
-                if piece.color == PieceColor::White {
-                    materials.white_material.clone()
-                } else {
-                    materials.black_material.clone()
-                }
+
+    impl FromWorld for PieceMaterials {
+            fn from_world(world: &mut World) -> Self {
+                let mut materials = world.get_resource_mut::<Assets<StandardMaterial>>().unwrap();
+                PieceMaterials {
+                hover_entmatl : materials.add(Color::from(GREEN_300)),
+                white_material : materials.add(Color::from(AMBER_400)),
+                black_material : materials.add(Color::BLACK),
             }
         }
-
-
-        impl FromWorld for PieceMaterials {
-                fn from_world(world: &mut World) -> Self {
-                    let mut materials = world.get_resource_mut::<Assets<StandardMaterial>>().unwrap();
-                    PieceMaterials {
-                    hover_entmatl : materials.add(Color::from(GREEN_300)),
-                    white_material : materials.add(Color::from(AMBER_400)),
-                    black_material : materials.add(Color::BLACK),
-                }
-            }
-        }
+    }
         
 
-        #[derive(Debug, Resource)]
-        pub struct ReturnMaterials;
+    #[derive(Debug, Resource)]
+    pub struct ReturnMaterials;
 
-        impl Default for ReturnMaterials {
-            fn default() -> Self {
-                ReturnMaterials
+    impl Default for ReturnMaterials {
+        fn default() -> Self {
+            ReturnMaterials
+        }
+    }
+
+    impl ReturnMaterials {
+        pub fn get_original_material(&self, square: &Square, materials: &SquareMaterials) -> Handle<StandardMaterial> {
+            if square.is_white() {
+                materials.black_color.clone()
+            } else {
+                materials.white_color.clone()
             }
         }
-
-        impl ReturnMaterials {
-            pub fn get_original_material(&self, square: &Square, materials: &SquareMaterials) -> Handle<StandardMaterial> {
-                if square.is_white() {
-                    materials.black_color.clone()
-                } else {
-                    materials.white_color.clone()
-                }
-            }
-        }
+    }
 
          
     
